21443b71d56b7e2cbfff96279eae31449e2a6174
cdgarcia <cesardariogar@gmail.com>
2025-10-11 21:21:09 -0700

    add patient-billing base project module to patient-manager

---
+++ patient-billing/src/main/java/com/metalworkshop/patient_billing/PatientBillingApplication.java
@@ -0,0 +1,13 @@
+package com.metalworkshop.patient_billing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientBillingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientBillingApplication.class, args);
+	}
+
+}
---
+++ patient-billing/src/test/java/com/metalworkshop/patient_billing/PatientBillingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metalworkshop.patient_billing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientBillingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
