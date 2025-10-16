ffe0c2dffe54825e0daa2684a9cf8cf73d1f3c13
cdgarcia <cesardariogar@gmail.com>
2025-10-15 23:05:48 -0700

    update patient-event package name and fix imports

--- patient-service/src/main/java/com/metalworkshop/PatientService/kafka/KafkaProducer.java
+++ patient-service/src/main/java/com/metalworkshop/PatientService/kafka/KafkaProducer.java
@@ -5,7 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
-import patient.event.PatientEvent;
+import patient.events.PatientEvent;
 
 @Service
 public class KafkaProducer {
