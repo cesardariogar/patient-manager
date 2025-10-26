ec605792fe13d966ba167e6bc4ff767af85713ee
cdgarcia <cesardariogar@gmail.com>
2025-10-25 23:30:14 -0700

    add JwtValidationGatewayFilterFactory to api-gateway plus auth url env var

--- api-gateway/src/test/java/com/metalworkshop/api_gateway/ApiGatewayApplicationTests.java
+++ api-gateway/src/test/java/com/metalworkshop/api_gateway/ApiGatewayApplicationTests.java
@@ -2,8 +2,12 @@ package com.metalworkshop.api_gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+        "auth.service.url="
+})
 class ApiGatewayApplicationTests {
 
 	@Test
