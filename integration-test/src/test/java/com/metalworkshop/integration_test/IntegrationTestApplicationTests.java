9451c2c42c2762edcdc08fc89037c4d34322239a
cdgarcia <cesardariogar@gmail.com>
2025-11-06 23:50:27 -0700

    integration tests project created

---
+++ integration-test/src/test/java/com/metalworkshop/integration_test/IntegrationTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metalworkshop.integration_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegrationTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
