8fb979bee4fe06ccbd6f101685f7267a097fb04f
Cesar D. Garcia <cesardariogar@gmail.com>
2025-12-01 22:25:55 -0700

    update docker-compose file to inclue memory limits to reduce footprint and being able to run this in my shitty laptop

---
+++ docker/scripts/init-db/02-replace-back.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+echo "Restoring SQL initialization scripts..."
+
+# Paths to secrets
+PATIENT_PASS=$(cat /run/secrets/DB_PATIENT_PASSWORD)
+AUTH_PASS=$(cat /run/secrets/DB_AUTH_PASSWORD)
+
+# Replace placeholders inside SQL scripts
+sed -i "s/${PATIENT_PASS}/REPLACE_PATIENT_PASSWORD/g" /docker-entrypoint-initdb.d/01-init.sql
+sed -i "s/${AUTH_PASS}/REPLACE_AUTH_PASSWORD/g" /docker-entrypoint-initdb.d/01-init.sql
+
+echo "Init scripts variables restored."
+
+exec "$@"
