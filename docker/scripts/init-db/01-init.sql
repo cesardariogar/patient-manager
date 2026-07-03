311419065f7729aca712ab6232f2f2a0f5156072
Cesar Dario Garcia <cesardariogar@gmail.com>
2026-07-02 21:18:22 -0700

    update database names for auth, and patient for a clarity

--- docker/scripts/init-db/01-init.sql
+++ docker/scripts/init-db/01-init.sql
@@ -1,16 +1,16 @@
 -- PATIENT SERVICE DATABASE
-CREATE DATABASE patient_service;
-ALTER DATABASE patient_service OWNER TO patient_admin;
-\connect patient_service;
+CREATE DATABASE patient_service_db;
+ALTER DATABASE patient_service_db OWNER TO patient_admin;
+\connect patient_service_db;
 GRANT ALL PRIVILEGES
-    ON DATABASE patient_service
+    ON DATABASE patient_service_db
     TO patient_admin;
 
 
 -- AUTH SERVICE DATABASE
-CREATE DATABASE auth_service;
-ALTER DATABASE auth_service OWNER TO auth_admin;
-\connect auth_service;
+CREATE DATABASE auth_service_db;
+ALTER DATABASE auth_service_db OWNER TO auth_admin;
+\connect auth_service_db;
 GRANT ALL PRIVILEGES
-    ON DATABASE auth_service
+    ON DATABASE auth_service_db
     TO auth_admin;
